678f92d9fa7168b750cc91447ce8e22f7b6ff266
Ахметов Рустам <ahmetov.rustam2011@gmail.com>
2023-10-04 21:58:18 +0500

    select command

---
+++ src/commands/select.command.ts
@@ -0,0 +1,23 @@
+import { Command } from "./command.class";
+import { Markup, Telegraf } from "telegraf";
+import { IBotContext } from "../context/context.interface";
+
+export class SelectCommand extends Command {
+  constructor(bot: Telegraf<IBotContext>) {
+    super(bot);
+  }
+
+  handle(): void {
+    this.bot.command("select", (ctx) => {
+      ctx.reply(
+        "Выберите для кого рассказать сказку",
+        Markup.inlineKeyboard([
+          Markup.button.callback("Мальчику", "story_for_boy"),
+          Markup.button.callback("Девочке", "story_for_girl"),
+          Markup.button.callback("Мальчику и девочке", "story_for_both"),
+          Markup.button.callback("Случайно", "random_story"),
+        ]),
+      );
+    });
+  }
+}
