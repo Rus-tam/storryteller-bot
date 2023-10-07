42363b2aae73ff9bcefbaaa882c0a4e7418f67ca
Ахметов Рустам <ahmetov.rustam2011@gmail.com>
2023-10-08 02:15:11 +0500

    session refactoring

--- src/context/context.interface.ts
+++ src/context/context.interface.ts
@@ -3,6 +3,7 @@ import { Context } from "telegraf";
 export interface ISessionData {
   storyType: string;
   storyIdForAdmit: number;
+  isAdmin: boolean;
 }
 
 export interface IBotContext extends Context {
